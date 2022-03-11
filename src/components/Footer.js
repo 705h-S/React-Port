8236b277f7333c420a519a4cedfc7b0703dc3878
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-10 20:22:24 -0800

    added font awsome and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,26 @@
-import React from 'react';
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="col d-flex flex-row justify-content-center" id="footer">
+      <a
+        className="mx-3"
+        href="https://github.com/705h-S"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-github fa-3x"></i>
+      </a>
+      <a
+        className="mx-3"
+        href="https://www.linkedin.com/in/joshua-meza-sinai/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-linkedin fa-3x"></i>
+      </a>
+    </footer>
+  );
+}
 
 export default Footer;
