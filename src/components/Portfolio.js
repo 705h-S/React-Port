540d741d202436ee29489dba98a862122eaa5cc0
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-10 19:53:56 -0800

    imported pages to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,3 +1,6 @@
 import Nav from './Nav';
 import Header from './Header';
 import Footer from './Footer';
+import Resume from './pages/Resume';
+import Project from './pages/Project';
+import About from './pages/About';
