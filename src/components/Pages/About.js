e0d16cd9d00dc596787b740bd1f06588735ea4bf
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-10 20:34:40 -0800

    added about me

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,3 +1,31 @@
-import React from 'react';
+import React from "react";
+function About() {
+  return (
+    <div
+      className=" mx-3 my-3 "
+      id="about-me"
+      style={{ paddingBottom: "2.5rem" }}
+    >
+      <h1 className="display-3 font-weight-bold d-flex justify-content-center">
+        About Me
+      </h1>
+      <div className="row d-flex justify-content-center flex-wrap">
+        <div className="col-sm-12 col-md-6 col-lg-6">
+          <img src="./ava.png" alt="portrait" height="500" />
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-6">
+          <p className="lead" id="aboutme">
+            Hello, My Name is Joshua Meza, and I aspire to be a full stack web
+            developer. My interest in web development started in highschool when
+            I figured out that I can change the text on a site with the inspect
+            tool. Fast-forward to today and I'm already building web
+            applications and learning all sorts of technology.
+          </p>
+        </div>
+      </div>
 
+      <hr className="my-4" />
+    </div>
+  );
+}
 export default About;
