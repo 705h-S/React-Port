b7b4af530768d07d2f917f5180fd2532023eb098
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-10 20:19:16 -0800

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,12 @@
-import React from 'react';
+import React from "react";
+import Nav from "./Nav";
 
+function Header({ currentPage, handlePageChange }) {
+  return (
+    <header className="d-inline-flex flex-column space-between px-1 w-100 align-items-center align-center justify-content-around">
+      <div className="h2">Joshua Meza</div>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+    </header>
+  );
+}
 export default Header;
