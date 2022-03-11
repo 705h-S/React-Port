ed97b165ce92c63e4a06de9e8888cdf7f202864a
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-10 21:06:29 -0800

    added resume

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -1,3 +1,16 @@
 import React from "react";
-
+function Resume() {
+  return (
+    <iframe
+      title="resume"
+      src="./JoshuaMeza.pdf"
+      style={{
+        width: "100%",
+        height: "100%",
+        border: "none",
+        paddingBottom: "2.5rem",
+      }}
+    ></iframe>
+  );
+}
 export default Resume;
