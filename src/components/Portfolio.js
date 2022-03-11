9d1c3c8d99b8f4b88f6dfc6d93ed9879024ca827
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-10 21:09:26 -0800

    finished portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,46 @@
-import Nav from './Nav';
-import Header from './Header';
-import Footer from './Footer';
-import Resume from './pages/Resume';
-import Project from './pages/Project';
-import About from './pages/About';
+import Nav from "./Nav";
+import Header from "./Header";
+import Footer from "./Footer";
+import Resume from "./pages/Resume";
+import Project from "./pages/Project";
+import About from "./pages/About";
+import React, { useState } from "react";
+const styles = {
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "gray",
+  },
+};
+
+function Portfolio() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Project") {
+      return <Project />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div
+      style={{ background: "#050f21", color: "#8bace8", height: "120vh" }}
+      id="portfolio"
+    >
+      <div style={styles.header}>
+        <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      </div>
+      {renderPage()}
+      {!currentPage === "Resume" ? <Footer /> : <></>}
+    </div>
+  );
+}
+export default Portfolio;
