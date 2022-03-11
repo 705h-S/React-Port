868688e3d63f387dc348161bacdf5004e9282a9e
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-10 20:12:37 -0800

    added nav com

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,44 @@
-import React from 'react';
+import React from "react";
+function Nav({ currentPage, handlePageChange }) {
+  return (
+    <nav className="main-header-menu d-flex list-group w-100">
+      <ul className="nav nav-tabs space-between align-items-center d-flex flex-row justify-content-center">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={
+              currentPage === "About" ? "nav-link text-white" : "nav-link"
+            }
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#project"
+            onClick={() => handlePageChange("Project")}
+            className={
+              currentPage === "Project" ? "nav-link text-white" : "nav-link"
+            }
+          >
+            Projects
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={
+              currentPage === "Resume" ? "nav-link text-white" : "nav-link"
+            }
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Nav;
